f4aed6fcecc68019fa7a2ca23043c5cf2360d676
McRaeAlex <alex@alexandermcrae.com>
2019-06-23 18:15:37 -0700

    wrote basic code

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from Board import Board
 
 if __name__ == '__main__':
-    print('Hi')
+    # 1. Ask user for input (ipaddr) to connect too or 2. if they are expecting a game or 3. play alone
+    # 1.
+    # attempt to connect to that server
+    # if failure inform user and crash
+    # else play the game
+    # 2.
+    # display ipaddr
+    # wait until a conntection is requested
+    # play game
+    # 3. Play game
+
     b = Board()
     print(b)
     while not b.isOver():
